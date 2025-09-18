b01f9fb03118ca57bb9b191ebdb9a8f7ad232ea3
TaeO <kto@gravityneocyon.com>
2025-09-18 12:49:47 +0900

    t

---
+++ GitDepth.bat
@@ -0,0 +1,12 @@
+:: MD D:\_ProjectS\ProjectS_Test
+:: CD D:\_ProjectS\ProjectS_Test
+:: git config --global user.name TaeO
+:: git config --global user.email kto@gravityneocyon.com
+:: git config credential.http://gitea.provider=generic
+git config --list
+:: git clone http://gitea/Client/ProjectS_Client.git --depth 1
+:: git clone http://gitea/Client/ProjectS_Client.git --depth 60
+:: cd ProjectS_Client
+:: git fetch --unshallow
+
+pause
